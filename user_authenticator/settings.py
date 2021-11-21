1277e7644876fde568c1f74eddf16499356a2b74
Aliz-f <fad127alireza2@gmail.com>
2021-11-21 13:15:29 +0330

    Add celery(with redis) for app

--- user_authenticator/settings.py
+++ user_authenticator/settings.py
@@ -28,6 +28,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+#*** Celery Settings:
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_TRANSPORT = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_BEAT_SCHEDULE = {}
+
+
 # Application definition
 
 INSTALLED_APPS = [
